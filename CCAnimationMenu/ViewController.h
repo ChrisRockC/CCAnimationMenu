851ba8cd90c1ec6d214513acc0440eac1476fe39
chenchaocc <ccerdong@sina.cn>
2015-07-27 02:50:21 +0800

    Initial Commit

---
+++ CCAnimationMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCAnimationMenu
+//
+//  Created by mac on 15/7/27.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
